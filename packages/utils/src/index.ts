806f601529fc4e0251990884ea44d9caeba2c481
Nimesh Nayaju <nimeshnayaju@gmail.com>
2024-06-18 12:46:12 -0400

    Implement editor serialization

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,1 +1,26 @@
-export {};
+export { $serializeEditor } from "./serialize-editor";
+export type { SerializedEditorState } from "./serialize-editor";
+
+export { isJsonArray, isJsonObject, isJsonScalar } from "./serialize-node";
+
+export type {
+  SerializedLexicalNode,
+  SerializedRootNode,
+  SerializedElementNode,
+  SerializedDecoratorNode,
+  SerializedLineBreakNode,
+  SerializedTextNode,
+  Json,
+  JsonScalar,
+  JsonArray,
+  JsonObject,
+} from "./serialize-node";
+
+export type {
+  SerializedPoint,
+  SerializedBaseSelection,
+  SerializedRangeSelection,
+  SerializedNodeSelection,
+  SerializedUnknownSelection,
+  SerializedSelection,
+} from "./serialize-selection";
